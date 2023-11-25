ef3d839c59b9720e8198555f0d86c93ca5ceb1e7
Kamil Szostek <szostekamil19@gmail.com>
2023-11-25 20:51:11 +0100

    daily

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -15,31 +15,36 @@ export const authOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        if (credentials) {
-          const client = await MongoClient.connect(`${process.env.MONGODB_CONNECTION_STRING}`
-          );
-          const users = client?.db().collection("ToDoUsers");
-          const result = await users.findOne({
-            login: credentials.login,
-          });
-          if (!result) {
+        try{
+          if (credentials) {
+            const client = await MongoClient.connect(`${process.env.MONGODB_CONNECTION_STRING}`
+            );
+            const users = client?.db().collection("ToDoUsers");
+            const result = await users.findOne({
+              login: credentials.login,
+            });
+            if (!result) {
+              client.close();
+              throw new Error("User didn't found with that login");
+            }
+            if (
+              decrypt(result.password, `${process.env.CRYPTO_SECRET}`) !==  credentials.password
+            ) {
+              client.close();
+              throw new Error("Password doesn't match.");
+            }
             client.close();
-            throw new Error("User didn't found with that login");
-          }
-          if (
-            decrypt(result.password, `${process.env.CRYPTO_SECRET}`) !==  credentials.password
-          ) {
-            client.close();
-            throw new Error("Password doesn't match.");
-          }
-          client.close();
-          return {
-            id: result._id.toString(),
-            email: "",
-            name: result.login,
-            image: "",
-          };
-        } else return {login: '', id: ''};
+            return {
+              id: result._id.toString(),
+              email: "",
+              name: result.login,
+              image: "",
+            };
+          } else return null;
+        }
+        catch(err){
+          throw err;
+        }
       },
     }),
   ],
